5db27514fcead2155e0e7f17d1b98eeceeff971d
Mitty0719 <sprout071900@gmail.com>
2022-04-30 18:26:09 +0900

    Add ItemScreen file and DOM structure

---
+++ src/components/specific/ItemScreen.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <section class="speific-item-section">
+      <article class="info-article">
+        <div class="img-box"></div>
+        <div class="text-box">
+          <h3 class="title"></h3>
+          <div class="">
+            <div>
+              <div></div>
+              <div></div>
+            </div>
+            <div>
+              <span></span> / <span></span>
+            </div>
+          </div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <p>
+            
+          </p>
+        </div>
+      </article>
+      <article class="exp-article"></article>
+      <article class="like-article"></article>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scope>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import { createWebHistory, createRouter } from "vue-router";
 import MainScreen from "./components/main/MainScreen.vue";
 import MenuScreen from "./components/menu/MenuScreen.vue";
+import ItemScreen from "./components/specific/ItemScreen.vue";
 const routes = [
   {path:'/', component: MainScreen},
-  {path:'/menu', component: MenuScreen}
+  {path:'/menu', component: MenuScreen},
+  {path:'/item', component: ItemScreen}
 ]; // route config - 라우팅패스, 컴포넌트 정의
 
 const router = createRouter({
